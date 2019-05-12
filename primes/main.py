3e537fb6a22158df09b51fdf993c3ca37f9924b6
Ethan Bensman <ebenz99@gmail.com>
2019-05-12 00:00:22 -0400

    final conclusions

--- primes/main.py
+++ primes/main.py
@@ -1,4 +1,19 @@
 from generator import *
+from math import log
 
-l = generate(10000)
-print(l)
+l = generate(1000000)
+
+cp = 10
+
+cpCount = {}
+
+cpCount[1] = 0
+
+for num in l:
+	if num/cp == 0:
+		cpCount[round(log(cp,10))] += 1
+	else:
+		cp*=10
+		cpCount[round(log(cp,10))] = 1
+
+print(cpCount)
